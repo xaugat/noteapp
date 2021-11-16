c9bdd963be346e38204351d470345ccd782d43ac
saugat poudel <saugat.poudel@riddhasoft.com>
2021-11-16 17:37:46 +0545

    sqflite started working on

--- todoapp/lib/model/note_model.dart
+++ todoapp/lib/model/note_model.dart
@@ -1,3 +1,41 @@
+
+import 'package:flutter/cupertino.dart';
+
+final String noteid = 'id';
+final String notetitle = 'title';
+final String notedescription = 'description';
+final String notestatus = 'done';
+final String notedate = 'date';
+
 class NoteModel{
+  int? id;
+  String? title;
+  String? description;
+  String? date;
+  String? status;
+
   
+
+    Map<String, Object?> toMap() {
+    var map = <String, Object?>{
+      notetitle: title,
+      notedescription: description,
+      notedate: date,
+      notestatus: status
+    };
+    if (id != null) {
+      map[noteid] = id;
+    }
+    return map;
+  }
+
+NoteModel();
+
+  NoteModel.fromMap(Map<dynamic, dynamic> map) {
+    id = map[noteid];
+    title = map[notetitle];
+    date = map[notedate];
+    status = map[notedate];
+  }
+
 }
--- todoapp/lib/viewmodel/notesprovider.dart
+++ todoapp/lib/viewmodel/notesprovider.dart
@@ -1,9 +1,53 @@
 import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:todoapp/model/note_model.dart';
 
 class NotesProvider extends ChangeNotifier{
 
-  addNotes(){
-    
+final String tableTodo = 'todo';
+final String columnId = 'id';
+final String columnTitle = 'title';
+final String columnDesc = 'description';
+final String columnDone = 'done';
 
+ Database? db;
+
+  Future open(String path) async {
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+create table $tableTodo ( 
+  $columnId integer primary key autoincrement, 
+  $columnTitle text not null,
+  $columnDone integer not null)
+''');
+    });
   }
+
+  Future<NoteModel> insert(NoteModel todo) async {
+    todo.id = await db!.insert(tableTodo, todo.toMap());
+    return todo;
+  }
+
+  Future<NoteModel> getTodo(int id) async {
+    List<Map> maps = await db!.query(tableTodo,
+        columns: [columnId, columnDone, columnTitle],
+        where: '$columnId = ?',
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return NoteModel.fromMap(maps.first);
+    }
+    return NoteModel.fromMap(maps.first);
+  }
+
+  // Future<int> delete(int id) async {
+  //   return await db.delete(tableTodo, where: '$columnId = ?', whereArgs: [id]);
+  // }
+
+  // Future<int> update(Todo todo) async {
+  //   return await db.update(tableTodo, todo.toMap(),
+  //       where: '$columnId = ?', whereArgs: [todo.id]);
+  // }
+
+  Future close() async => db!.close();
 }
